f3f326d140906483e08698cda03d458a3b9e62b6
Tobias Weiss <tweiss@it-economics.de>
2022-08-23 13:01:19 +0200

    added sliding for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SpendingMgmtComponent} from "./spending-mgmt/spending-mgmt.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', redirectTo: '/spending', pathMatch: 'full'},
+  {path: 'spending', component: SpendingMgmtComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SpendingMgmtComponent } from './spending-mgmt/spending-mgmt.component';
+import {AngularFullpageModule} from "@fullpage/angular-fullpage";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpendingMgmtComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    AngularFullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]

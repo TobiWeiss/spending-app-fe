91221e94ac5ffbcc3a349ffb4325c4591570745b
Tobias Weiss <tweiss@it-economics.de>
2022-08-25 12:58:26 +0200

    working state for page transitions and menu

--- src/app/core/components/side-menu/side-menu.component.ts
+++ src/app/core/components/side-menu/side-menu.component.ts
@@ -1,13 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {NavigationEnd, Route, Router} from "@angular/router";
+import {exportedRoutes} from "../../../app-routing.module";
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+  styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent implements OnInit {
   public menuEntries: Array<{ icon: string, label: string, path: string }> = [];
+  public activeRoute: Route = exportedRoutes[0];
+  public showLogo: string = "start";
 
   constructor(private router: Router) {
   }
@@ -18,6 +21,14 @@ export class SideMenuComponent implements OnInit {
       {icon: "pi-history", label: "Historie", path: "history"},
       {icon: "pi-chart-line", label: "Statistik", path: "statistics"}
     ]
+
+    this.router.events.subscribe((routerEvent) => {
+      if (routerEvent instanceof NavigationEnd) {
+        this.activeRoute = exportedRoutes.filter(route => `/${route.path}` === routerEvent.url)[0];
+      }
+    })
+
+
   }
 
 }
---
+++ src/app/routing-animation.ts
@@ -0,0 +1,42 @@
+import {animate, group, query, style, transition, trigger} from "@angular/animations";
+
+export const slider =
+  trigger('routeAnimations', [
+    transition('first => second', slideTo('bottom')),
+    transition('first => third', slideTo('top')),
+    transition('second => third', slideTo('bottom')),
+    transition('second => first', slideTo('top')),
+    transition('third => first', slideTo('bottom')),
+    transition('third => second', slideTo('top'))
+  ]);
+
+function slideTo(direction: string) {
+  const optional = {optional: true};
+  return [
+    query(':enter, :leave', [
+      style({
+        position: 'absolute',
+        left: 0,
+        [direction]: 0,
+        height: '100%',
+        width: '100%'
+      })
+    ], optional),
+    query(':enter', [
+      style({[direction]: '-100%'})
+    ]),
+    group([
+      query(':leave', [
+        animate('600ms ease', style({[direction]: '100%'}))
+      ], optional),
+      query(':enter', [
+        animate('600ms ease', style({[direction]: '0%'}))
+      ])
+    ]),
+    // Normalize the page style... Might not be necessary
+
+    // Required only if you have child animations on the page
+    // query(':leave', animateChild()),
+    // query(':enter', animateChild()),
+  ];
+}
